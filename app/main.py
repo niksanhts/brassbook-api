612774706c817094230d68735a0b7c47f9e93dcb
Niksanhts <Niksanhts@gmail.com>
2025-02-21 18:55:26 +0300

    add base sound comparing
    Добавил асинхронное сравнение звуковых файлов. Написано 11 unit-тестов.

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-print("hello")
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+
+from app.routes import router
+
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI()
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
